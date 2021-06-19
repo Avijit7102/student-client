9358648fdd88c8f7860f8be0d14ed05917b3889f
Avijit Das <dasavijit.aiub@gmail.com>
2021-06-19 10:36:25 -0400

    adding

--- src/components/AllStudents/AllStudents.js
+++ src/components/AllStudents/AllStudents.js
@@ -7,6 +7,18 @@ const AllStudents = () => {
             .then(res => res.json())
             .then(data => setAllStudent(data));
     }, [])
+
+    const handleDeleteButton = (id) => {
+        console.log("clicked", id);
+        fetch(`http://localhost:7700/deleteStudent/${id}`,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log("delete successfully!!!!!!!!");
+        })
+
+    }
     return (
         <div>
             <h4>All Students List</h4>
@@ -18,6 +30,7 @@ const AllStudents = () => {
                             <th scope="col">Age</th>
                             <th scope="col">Gender</th>
                             <th scope="col">Grade</th>
+                            <th scope="col">Delete</th>
                         </tr>
                     </thead>
                     {
@@ -28,6 +41,7 @@ const AllStudents = () => {
                                     <td>{st.Age}</td>
                                     <td>{st.Gender}</td>
                                     <td>{st.Grade}</td>
+                                    <td><button onClick={()=>handleDeleteButton(st._id)}>Delete</button></td>
                                 </tr>
 
                             </tbody>
